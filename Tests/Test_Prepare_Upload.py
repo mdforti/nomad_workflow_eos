8e988d6433197135fc6c44478b447387914cba47
Mariano Forti <marianodforti@gmail.com>
2023-02-19 09:54:15 +0100

    trying to copy test from nomad

--- Tests/Test_Prepare_Upload.py
+++ Tests/Test_Prepare_Upload.py
@@ -1,9 +1,11 @@
+import pdb
 import sys
 import json
 import os
 project_root = os.path.dirname(os.path.dirname(__file__))
 sys.path.insert(0, project_root)
-sys.path.insert(1, '/data/Anaconda/ml_datasets/lib/python3.9/site-packages')
+sys.path.insert(1, '/data/git/nomad-FAIR/')
+
 from nomad.client import parse, normalize_all
 from nomad.client.processing import parse
 
@@ -12,9 +14,10 @@ from nomad.client.processing import parse
 
 from Tools.PrepareUpload.PrepareUpload import UploadMaker
 
-first_outcar = 'ExampleUpload/R-AAAAAAAABBB/volume_relaxed/xc=PBE-PAW.E=450.dk=0.020/OUTCAR.0.975'
+first_outcar = 'ExampleUpload/R-AAAAAAAABBB/volume_relaxed/xc=PBE-PAW.E=450.dk=0.020/OUTCAR.1.000'
 import glob
 import unittest
+
 class TestPrepareUploads(unittest.TestCase):
 
     @classmethod
@@ -25,7 +28,15 @@ class TestPrepareUploads(unittest.TestCase):
     def test_makes_anarchive(self):
         firstdir = os.path.dirname(self.uploader.volume_energy_files[0])
         archives = self.uploader.parse_outcar(firstdir)
-        print(archives[0].workflow[0].type )
+        self.assertTrue(len(archives)>0)
+
+    def test_makes_entry_archive(self):
+        entry_archive =  self.uploader.init_entry_archive()
+        self.assertTrue(hasattr(entry_archive, 'run'))
+        print(entry_archive.run.__dict__.keys())
+
+    def test_defines_a_quantity(self):
+        parser = TextParser()
     
 
 
--- Tools/PrepareUpload/PrepareUpload.py
+++ Tools/PrepareUpload/PrepareUpload.py
@@ -3,7 +3,11 @@ import sys
 #sys.path.insert(0, '/data/git/nomad-lab-1.1.5/')
 sys.path.insert(0, '/data/git/nomad-FAIR/')
 from nomad.client.processing import parse
-
+from nomad.datamodel import EntryArchive, EntryMetadata
+from nomad.parsing.file_parser import TextParser, Quantity, ParsePattern,\
+    XMLParser, BasicParser
+from nomad.datamodel.context import Context, ClientContext
+from nomad.datamodel.metainfo.simulation.run import Run, Program
 root = os.path.dirname(os.path.dirname(__file__))
 
 import glob
@@ -16,10 +20,19 @@ class UploadMaker:
         self.template_archive_json_file = ''
 
     
+    def init_entry_archive(self, mainfile_out = 'archive.json'):
+        this_archive =  EntryArchive()
+        run = this_archive.m_create(Run)
+        this_archive.metadata = EntryMetadata(m_context=ClientContext(local_dir=os.curdir))
+        this_archive.metadata.mainfile = mainfile_out
+        return this_archive
+
     def parse_outcar(self, thedir:str):
         OUTCARS = glob.glob(thedir+'/OUTCAR*')
         archive = parse(OUTCARS[0], parser_name = 'parsers/vasp')
         return archive
+
+
 
 
 
