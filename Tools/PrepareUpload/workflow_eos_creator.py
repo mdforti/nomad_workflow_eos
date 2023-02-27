8d73481212b21a2688ec61516810fd03a4a6c8ed
Mariano Forti <marianodforti@gmail.com>
2023-02-27 21:26:54 +0100

    cleaned a little bit

--- Tools/PrepareUpload/workflow_eos_creator.py
+++ Tools/PrepareUpload/workflow_eos_creator.py
@@ -39,32 +39,39 @@ def parse_outcar(theoutcar:str) -> EntryArchive:
     archives = parse(theoutcar)
     return [run_normalize(archive) for archive in archives]
 
-def create_eos_workflow(OUTCAR_dir):
-    """Entry with mechanical properties."""
-
-    OUTCARS = glob.glob(OUTCAR_dir+'/OUTCAR*')
-    list_of_archives = [parse_outcar(thisoutcar) for thisoutcar in OUTCARS]
-    list_of_volumes, list_of_energies = get_energies_from_list_outcars(list_of_archives)
+def get_template_from_min_energy(list_of_outcars, list_of_energies):
     min_energy_pos = np.argmin(list_of_energies) 
-    templates = parse_outcar(OUTCARS[min_energy_pos])
-    workflow = templates[0].m_create(Workflow)
-    # EOS workflow
-    workflow.type = 'equation_of_state'
+    return parse_outcar(list_of_outcars[min_energy_pos])
+
+def make_eos_from_ev_curve(thevolumes : list, theenergies: list) -> EquationOfState:
     equation_of_state = EquationOfState(
-        volumes=list_of_volumes,
-        energies=list_of_energies
+        volumes=thevolumes,
+        energies=theenergies
     )
-    BM = ASE_EOS([ v*1e30 for v in list_of_volumes ],[e*6.24e18 for e in  list_of_energies ], eos='birchmurnaghan')
+    # numbers are unit conversion
+    BM = ASE_EOS([ v *1e30  for v in thevolumes ],[ e * 6.241509e+18 for e in  theenergies ], eos='birchmurnaghan')
     v0, e0, B = BM.fit()
     eos_fit = equation_of_state.m_create(EOSFit)
     eos_fit.function_name = 'murnaghan'
-    eos_fit.fitted_energies = list_of_energies
+    eos_fit.fitted_energies = theenergies
     eos_fit.bulk_modulus = B
-    workflow.equation_of_state = equation_of_state
+    return equation_of_state
+
+
+def create_eos_workflow(OUTCAR_dir) -> EntryArchive:
+    """Entry with mechanical properties."""
+    OUTCARS = glob.glob(OUTCAR_dir+'/OUTCAR*')
+    list_of_archives = [parse_outcar(thisoutcar) for thisoutcar in OUTCARS]
+    list_of_volumes, list_of_energies = get_energies_from_list_outcars(list_of_archives)
+    templates = get_template_from_min_energy(OUTCARS, list_of_energies)
+    workflow = templates[0].m_create(Workflow)
+    # EOS workflow
+    workflow.type = 'equation_of_state'
+    workflow.equation_of_state = make_eos_from_ev_curve(list_of_volumes, list_of_energies)
     workflow.calculations_ref = [archive[0].run[0].calculation[0] for archive in list_of_archives]
     return run_normalize(templates[0])
 
-def get_energies_from_list_outcars(list_of_archives):
+def get_energies_from_list_outcars(list_of_archives) -> list:
 #    list_of_archives = [parse_outcar(thisoutcar) for thisoutcar in list_of_outcars]
     list_of_energies = [archives[0].run[0].calculation[0].energy.total.value._magnitude for archives in list_of_archives]
 #    list_of_lattice_vectors = [archives[0].run[0].system[0].atoms.lattice_vectors for archives in list_of_archives]
